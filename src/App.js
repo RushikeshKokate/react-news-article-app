eb8fe3e4beac1332dbff28b73efa91ac3aa0fc8f
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-07-30 22:46:27 +0530

    All the predefine css is removed from app.css, BrowerRouter imported and implemented to wrap App.js and Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
- 
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
+       <Routes>
+        <Route path='/' />
+        <Route/>
+       </Routes>
     </div>
   );
 }
